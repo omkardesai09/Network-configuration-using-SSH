b1646d61691cf37dd559d1c4131efcf317968d61
omkar <omkardesai09>
2018-04-05 14:51:45 -0700

    Check device's reachability using subprocess.call method

--- ssh_nw_config.py
+++ ssh_nw_config.py
@@ -1,6 +1,7 @@
 import paramiko
 import time
 import re
+import subprocess
 
 # To check IP addresses mentioned in file are valid or not
 def valid_ip():
@@ -26,12 +27,40 @@ def valid_ip():
     for ip in list_of_ip:
         ip_octets = ip.split('.')
         ip_octets[3] = ip_octets[3].strip('\n')
-        print ip_octets
+        #print ip_octets
         if (len(ip_octets) == 4) and (1 <= int(ip_octets[0]) <= 223) and (int(ip_octets[0]) != 127) and (int(ip_octets[0]) != 169 or int(ip_octets[1]) != 254) and (0 <= int(ip_octets[1]) <= 255 and 0 <= int(ip_octets[2]) <= 255 and 0 <= int(ip_octets[3]) <= 255):
             continue
 
         else:
             print "Invaid IP address: %s \n Please correct." %str(".".join(ip_octets))
             continue
+
+    # Check device's reachability using subprocess.call method
+
+    print "\n Code is checking all device's reachability.\n Please wait...\n"
+
+    flag = False
+    for ip in list_of_ip:
+        ping_reply = subprocess.call(['ping', '-c', '2', '-w', '2', '-q', '-n', ip])
+
+        if ping_reply == 0:
+            flag = True
+            continue
+
+        elif ping_reply == 1 or ping_reply == 2:
+            flag = False
+            break
+
+        else:
+            flag = False
+            break
+
+    if flag == True:
+        print "Awesome!!! All devices are reachable.\n"
+
+    elif flag == False:
+        print "Oh! Devices are not reachable. Please check IP addresses and connectivity again.\n"
+        #valid_ip()
+
 
 valid_ip()
